e0aee5e5562104f0bb86b3190463bdb4000e7103
zhengger <zhengger@gmail.com>
2023-10-07 14:48:33 +0800

    build: update  motion-module link

--- scripts/download/01-Motion-Modules.sh
+++ scripts/download/01-Motion-Modules.sh
@@ -3,8 +3,9 @@
 echo "Attempting download of Motion Module models from Google Drive."
 echo "If this fails, please download them manually from the links in the error messages/README."
 
-gdown 1RqkQuGPaCO5sGZ6V6KZ-jUWmsRu48Kdq -O models/motion-module/ || true
-gdown 1ql0g_Ys4UCz2RnokYlBjyOYPbttbIpbu -O models/motion-module/ || true
+# gdown 1RqkQuGPaCO5sGZ6V6KZ-jUWmsRu48Kdq -O models/motion-module/ || true
+# gdown 1ql0g_Ys4UCz2RnokYlBjyOYPbttbIpbu -O models/motion-module/ || true
+wget https://civitai.com/api/download/models/159987 -P models/motion-module/ --content-disposition --no-check-certificate
 
 echo "Motion module download script complete."
 echo "If you see errors above, please download the models manually from the links in the error messages/README."
