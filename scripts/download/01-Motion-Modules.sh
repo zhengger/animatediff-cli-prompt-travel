7e28d1ef32d7008095f336435392a23bfdb8707e
zhengger <zhengger@gmail.com>
2023-10-07 15:36:59 +0800

    build: v0.0.1

--- scripts/download/01-Motion-Modules.sh
+++ scripts/download/01-Motion-Modules.sh
@@ -5,7 +5,7 @@ echo "If this fails, please download them manually from the links in the error m
 
 # gdown 1RqkQuGPaCO5sGZ6V6KZ-jUWmsRu48Kdq -O models/motion-module/ || true
 # gdown 1ql0g_Ys4UCz2RnokYlBjyOYPbttbIpbu -O models/motion-module/ || true
-wget https://civitai.com/api/download/models/159987 -P models/motion-module/ --content-disposition --no-check-certificate
+wget https://civitai.com/api/download/models/159987 -P /content/animatediff-cli-prompt-travel/data/models/motion-module --content-disposition --no-check-certificate
 
 echo "Motion module download script complete."
 echo "If you see errors above, please download the models manually from the links in the error messages/README."
