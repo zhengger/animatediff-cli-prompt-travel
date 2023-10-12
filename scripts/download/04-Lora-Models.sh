177dfd6369186916bc69cf65cdc629509a5a1218
zhengger <zhengger@gmail.com>
2023-10-12 13:44:10 +0800

    build: v0.0.1

--- scripts/download/04-Lora-Models.sh
+++ scripts/download/04-Lora-Models.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 wget https://huggingface.co/OedoSoldier/detail-tweaker-lora/resolve/main/add_detail.safetensors -P ../../data/loras/ --content-disposition --no-check-certificate
 ! wget https://civitai.com/api/download/models/73657 -P ../../data/loras --content-disposition --no-check-certificate
-! wget https://civitai.com/api/download/models/73657 -P ../../data/loras --content-disposition --no-check-certificate
+! wget https://civitai.com/api/download/models/157663 -P ../../data/loras --content-disposition --no-check-certificate
 # wget https://huggingface.co/OedoSoldier/detail-tweaker-lora/resolve/main/add_detail.safetensors -P ../../data/models/loras/ --content-disposition --no-check-certificate
